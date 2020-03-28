7086d21c5c4d951cb3953c246365e74ea0babf7b
enes <enuxuna@gmail.com>
2020-03-28 21:41:48 +0300

    created home

---
+++ src/components/Home/Home.styled.js
@@ -0,0 +1,30 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+
+    flex-direction: column;
+    align-self: stretch;
+    flex: 1;
+    flex-grow: 1;
+    justify-content: center;
+    align-items: center;
+`
+
+const Button = styled.button`
+    min-width: 250px;
+
+    padding: 10px 40px;
+    margin: 10px;
+    border: 1px solid #cdcdcd;
+    border-radius: 5px;
+
+    font-size: 48px;
+    background-color: ${props => props.backgroundColor || "#fff"};
+    color: #fff;
+`
+
+export {
+    Button,
+    Container
+}
